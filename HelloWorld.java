5a2c42e2d92f2bdcf4958a417146beb5fa95c615
Landyn Martin <landyn.martin@mrjaffesclass.com>
2022-09-15 14:14:03 -0700

    Commit 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,9 @@ public class HelloWorld
 {
     public static void main(String[] args) 
     {
+        int i;
+        double f = 2.5;
+        boolean flag = true;
         System.out.println("Hello World!");
     }
 }
